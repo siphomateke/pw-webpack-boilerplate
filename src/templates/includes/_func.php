2a249ac879020255f7a48ded7f1f64be7d9a4dfe
nextgensparx <siphomateke@gmail.com>
2017-12-24 22:16:07 +0200

    Improve chunk loading

--- src/templates/includes/_func.php
+++ src/templates/includes/_func.php
@@ -5,12 +5,40 @@ include("./_assets-manifest.php");
 /**
  * Get's the filename of a file built by webpack based on the chunk name
  */
-function getWebpackBuiltFile($type, $chunkName) {
-    $file = "";
+function getWebpackChunk($type, $chunkName) {
+    $array = null;
     if ($type == "js") {
-        $file = WebpackBuiltFiles::$jsFiles[$chunkName];
+        $array = WebpackBuiltFiles::$jsFiles;
     } else if ($type == "css") {
-        $file = WebpackBuiltFiles::$cssFiles[$chunkName];
+        $array = WebpackBuiltFiles::$cssFiles;
     }
-    return wire("config")->urls->root.$file;
+
+    if (array_key_exists($chunkName, $array)) {
+        return wire("config")->urls->root.$array[$chunkName];
+    } else {
+        return null;
+    }
+}
+
+function loadWebpackChunk($type, $chunkName) {
+    $chunk = getWebpackChunk($type, $chunkName);
+    if (!is_null($chunk)) {
+        if ($type == "js") {
+            return "<script type='text/javascript' src='".$chunk."'></script>";
+        } else if ($type == "css") {
+            return "<link rel='stylesheet' type='text/css' href='".$chunk."'>";
+        }
+    }
+    return null;
+}
+
+function loadWebpackChunks($type, $chunkNames) {
+    $str = "";
+    foreach ($chunkNames as $chunkName) {
+        $result = loadWebpackChunk($type, $chunkName);
+        if (!is_null($result)) {
+            $str .= $result;
+        }
+    }
+    return $str;
 }
--- src/templates/includes/_main.php
+++ src/templates/includes/_main.php
@@ -5,9 +5,7 @@
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title data-pw-id='title'>Title</title>
 
-        <script type='text/javascript' src='<?=getWebpackBuiltFile("js","manifest")?>'></script>
-        <script type='text/javascript' src='<?=getWebpackBuiltFile("js","main")?>'></script>
-        <link rel='stylesheet' type='text/css' href='<?=getWebpackBuiltFile("css","main")?>'>
+        <?=loadWebpackChunk("css","main")?>
 
         <region data-pw-id='head'></region>
     </head>
@@ -15,6 +13,12 @@
     <body>
         <main data-pw-id='main'></main>
         <footer data-pw-id='footer'></footer>
-        <region data-pw-id='scripts'></region>
+        <region data-pw-id='scripts'>
+            <?php
+            echo loadWebpackChunks("js", array(
+                "manifest", "vendor", "main"
+            ));
+            ?>
+        </region>
     </body>
 </html>
